99cdf547a5d6947d7a8f5693d04cab95184d666f
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-20 16:38:24 +0600

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            double taxRate = 0.15;
+            double taxedSalary = salary - (salary * taxRate);
+            System.out.println("Salary after taxes: " + taxedSalary);
+        } else if (salary <= 20000) {
+            double taxRate = 0.18;
+            double taxedSalary = salary - (salary * taxRate);
+            System.out.println("Salary after taxes: " + taxedSalary);
+        } else {
+            double taxRate = 0.20;
+            double taxedSalary = salary - (salary * taxRate);
+            System.out.println("Salary after taxes: " + taxedSalary);
+        }
     }
+
+  
 }
+
