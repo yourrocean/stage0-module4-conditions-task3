97d1dabb56b1f28086f6c523a9d305bf55c1bae1
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-20 17:40:00 +0600

    Update DaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                System.out.println("31 days");
+                break;
+            case 4: case 6: case 9: case 11:
+                System.out.println("30 days");
+                break;
+            case 2:
+                System.out.println("28 or 29 days");
+                break;
+            default:
+                System.out.println("wrong number!");
+                break;
+        }
     }
+
+ 
 }
+
