7f8b5cf463b673bb4fbdfa96d85bd3b38566a815
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-20 17:44:48 +0600

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -1,3 +1,4 @@
+package school.mjc.stage0.conditions.task3;
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
         if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z')) {
